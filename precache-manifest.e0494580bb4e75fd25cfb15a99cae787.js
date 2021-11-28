38e45267d2fa733e9f378b3b82b6e1da47bdde6e
Regina <88785730+Regina5425@users.noreply.github.com>
2021-11-28 18:07:25 +0300

    Добавлены файлы

---
+++ precache-manifest.e0494580bb4e75fd25cfb15a99cae787.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89b13a33c3cbd53cca10e14b709e7ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54576f681ff3a6a71cff",
+    "url": "/js/app.4caa2b5f.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
